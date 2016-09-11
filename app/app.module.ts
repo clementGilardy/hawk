fc74880439811e77f0bb0e5274b0938c7f46ff0b
clementGilardy <gilardy.clement@gmail.com>
2016-09-11 12:30:48 +0200

    add lecteur twitch

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { routing } from  './app.routing';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
+import { routing } from  './app.routing';
 import { AppComponent }   from './component/app.component';
 
-
-
 @NgModule({
-    imports:      [ BrowserModule, routing ],
+    imports:      [ BrowserModule, routing, FormsModule, HttpModule ],
     declarations: [ AppComponent ],
     bootstrap:    [ AppComponent ]
 })
